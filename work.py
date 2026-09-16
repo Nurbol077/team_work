da9dfa71a25cdca4df305473ca6c5a47747f6f5d
Torogeldi <aiyl4ievi4.torogeldi@gmail.com>
2026-09-16 20:22:17 +0600

    тапшырма

--- work.py
+++ work.py
@@ -1,1 +1,15 @@
-"""Проект башталды"""
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+stud = Student("Asan", 26)
+
+print("Аты:", stud.name)
+print("Жашы:", stud.age)
+
+class Car:
+    def __init__(self, ):
+        self.color
+        self.brand
