0b001b2149cc15632aad1d5be0ea651f3a80d4a9
Daniel <buzurmankulovdaniela@gmail.com>
2026-09-16 20:11:53 +0600

    daniel_movie

--- work.py
+++ work.py
@@ -1,1 +1,11 @@
 """Проект башталды"""
+
+class Movie:
+    def __init__(self, title, year):
+        self.title = title
+        self.year = year
+    def info(self):
+        print(f'Title:{self.title}\nYear:{self.year}')
+
+m1 = Movie('Avengers: Endgame', 2019)
+print(m1.info())
